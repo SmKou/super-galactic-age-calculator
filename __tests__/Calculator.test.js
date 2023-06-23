7d9ffa404bf4caaeb29e6c6f7a60d786d426e3a7
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-23 08:33:26 -0700

    INITFAIL Calculator.getMercuryYears()

--- __tests__/Calculator.test.js
+++ __tests__/Calculator.test.js
@@ -5,5 +5,18 @@ describe("Calculator", () => {
         const age = 1;
         const calculator = new Calculator(age);
         expect(calculator.age).toBe(1);
+    });
+});
+
+describe("Calculator.getMercuryYears()", () => {
+    let calculator;
+
+    beforeEach(() => {
+        const age = 1;
+        calculator = new Calculator(age);
+    });
+
+    test("Should return a number of years in Mercury years", () => {
+        expect(calculator.getMercuryYears(calculator.age)).toBeCloseTo(1 / 0.24);
     })
 })
--- src/js/bs/Calculator.js
+++ src/js/bs/Calculator.js
@@ -2,4 +2,16 @@ export default class Calculator {
     constructor(age) {
         this.age = age;
     }
- }
+
+    getMercuryYears(n) { }
+    
+    getVenusYears(n) { }
+    
+    getMarsYears(n) { }
+
+    getJupiterYears(n) { }
+    
+    getPastYears(n, planet) { }
+
+    getFutureYears(n, planet) {}
+}
